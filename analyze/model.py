d7afc0bba93c5028662bc1beb96e96a039747a9d
graham <graham@willsfamily.org>
2023-07-07 15:55:30 -0500

    speed improvement

--- analyze/model.py
+++ analyze/model.py
@@ -1,47 +1,52 @@
 from __future__ import annotations
 
 import logging
+import math
+import multiprocessing
 import pickle
 from dataclasses import dataclass, field
-
-logging.basicConfig(level='DEBUG')
-LOGGER = logging.getLogger('analyze')
-LOGGER.propagate = False
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-LOGGER.addHandler(handler)
+from datetime import datetime
+from functools import partial
+from multiprocessing.shared_memory import SharedMemory
 
 HARD: bool = True
-
-# These are the words we will use
-LOGGER.info('READING WORDS')
-with open('../resources/words.pickle', 'rb') as f:
-    WORDS = pickle.load(f)
-N_WORDS = len(WORDS)
-LOGGER.info(f'READ {N_WORDS} WORDS')
-
-# Pickled outcomes
-LOGGER.info('READING PRE_CALCULATED OUTCOMES')
-with open('../resources/outcomes.pickle', 'rb') as f:
-    OUTCOMES: [str] = pickle.load(f)
-LOGGER.info(f'READ {len(OUTCOMES)} PRE_CALCULATED OUTCOMES')
-
-# Pickled scores
-LOGGER.info('READING PRE_CALCULATED SCORES')
-with open('../resources/scores.pickle', 'rb') as f:
-    SCORES: bytes = pickle.load(f)
-LOGGER.info(f'READ {len(SCORES)} PRE_CALCULATED SCORES')
+MAX_CANDIDATES = 5
 
 
-def pp(bb: [int]) -> str:
-    return '|'.join(WORDS[b] for b in bb)
+class WordleContext:
+    words: [str]
+    n_words: int
+    outcomes: [str]
+    scores: bytes
+    shared_scores: SharedMemory
+
+    def __init__(self):
+        # These are the words we will use
+        with open('../resources/words.pickle', 'rb') as f:
+            self.words = pickle.load(f)
+        self.n_words = len(self.words)
+
+        # Pickled outcomes
+        with open('../resources/outcomes.pickle', 'rb') as f:
+            self.outcomes: [str] = pickle.load(f)
+
+        # Pickled scores
+        with open('../resources/scores.pickle', 'rb') as f:
+            self.scores: bytes = pickle.load(f)
+
+        self.shared_scores = SharedMemory(create=True, size=len(self.scores))
+        self.shared_scores.buf[:len(self.scores)] = self.scores
 
 
-def evaluate_possibilities(guess: int, possible: list[int], limit: int) -> [[int]] or None:
+def pp(bb: [int], ctx: WordleContext) -> str:
+    return '|'.join(ctx.words[b] for b in bb)
+
+
+def evaluate_possibilities(guess: int, possible: list[int], limit: int, scores: bytes, n_words: int) -> [[int]] or None:
     """ limit is the largest set to split allowed"""
     outcomes = [None] * 243
     for p in possible:
-        guess_result = SCORES[guess * N_WORDS + p]
+        guess_result = scores[guess * n_words + p]
         which = outcomes[guess_result]
         if not which:
             outcomes[guess_result] = which = []
@@ -58,11 +63,11 @@ class Segment:
     outcome: int
     node: Node
 
-    def display(self, depth: int = 0):
+    def display(self, ctx: WordleContext, depth: int = 0):
         leader = '  ' * (depth - 1) + ' '
-        print(leader, OUTCOMES[self.outcome], ': ', pp(self.node.possible), sep='')
+        print(leader, ctx.outcomes[self.outcome], ': ', pp(self.node.possible, ctx), sep='')
         if not self.node.only_one_possible():
-            self.node.display()
+            self.node.display(ctx)
 
     def only_one_possible(self):
         return self.node.only_one_possible()
@@ -84,17 +89,17 @@ class Node:
             mx = max(mx, max(s.node.max_guesses_needed() for s in segments))
         return 1 + mx
 
-    def display(self):
+    def display(self, ctx: WordleContext):
         leader = '  ' * self.depth
         if not self.guesses:
             print(leader, 'FINAL RESULT: ', pp(self.possible), sep='')
             return
 
-        print(leader, pp(self.possible), ' [', self.max_guesses_needed(), ']', sep='')
+        print(leader, pp(self.possible, ctx), ' [', self.max_guesses_needed(), ']', sep='')
         for g, seg in self.guesses:
-            print(leader, f"Guess@{self.depth}: {WORDS[g]}", sep='')
+            print(leader, f"Guess@{self.depth}: {ctx.words[g]}", sep='')
             for s in seg:
-                s.display(self.depth + 1)
+                s.display(ctx, self.depth + 1)
 
     def decides_outcome_immediately(self) -> bool:
         """ Don't need to follow any further; this guess solves it"""
@@ -122,76 +127,113 @@ class Node:
         self.guesses = [best[0]]
 
 
-def build_node_children(node: Node, max_children: int = 5) -> [Node]:
-    if HARD:
-        candidates = node.possible
-    else:
-        candidates = range(0, N_WORDS)
-    child_nodes = []
+def add_candidates_to_node(node: Node, best, needs_splitting: [Node]):
+    # Use the best ones
+    for _, c, outcomes in best:
+        segments = []
+        for k, v in enumerate(outcomes):
+            if v:
+                child = Node(v, depth=node.depth + 1)
+                segments.append(Segment(k, child))
+                if len(v) > 1:
+                    needs_splitting.append(child)
 
+
+def find_best_candidates(node, scores: memoryview, n_words: int) -> []:
+    candidates = node.possible
     # Sorted list, with the worst being the last
     best = []
-
     limit = 999999  # The worst we can tolerate
     for c in candidates:
-        outcomes = evaluate_possibilities(c, node.possible, limit)
+        outcomes = evaluate_possibilities(c, node.possible, limit, scores, n_words)
         if not outcomes:
             continue
 
         longest = max(len(o) for o in outcomes if o)
-        if len(best) < max_children:
+        if len(best) < 5:
+            # 5 best candidates
             best.append((longest, c, outcomes))
         elif longest < best[-1][0]:
             # This is better than the worst of our best, so it replaces
             limit = longest
             best[-1] = (longest, c, outcomes)
             best.sort(key=lambda x: x[:-1])
-
-    # Use the best ones
-    for _, c, outcomes in best:
-        segments = [Segment(k, Node(v, depth=node.depth + 1)) for k, v in enumerate(outcomes) if v]
-        node.guesses.append((c, segments))
-        child_nodes += [s.node for s in segments]
-    return child_nodes
+    return best
 
 
-def step_downward(nodes: [Node]) -> [Node]:
+def find_best_candidates_for_block(nodes: [Node], scores_mem: SharedMemory, n_words: int) -> []:
+    results = [find_best_candidates(n, scores_mem.buf, n_words) for n in nodes]
+    return results
+
+
+def step_downward(nodes: [Node], ctx: WordleContext) -> [Node]:
     n = len(nodes)
-    print(f'Processing {n} nodes: ', end='', flush=True)
+
+    t0 = datetime.now()
+    print(f'Processing {n} nodes', flush=True)
 
     next_nodes = []
-    last_percent = 0
-    for i, node in enumerate(nodes):
-        percent = (i * 1000 // n) * 0.1
-        if percent != last_percent:
-            if last_percent % 5:
-                print('.', end='', flush=True)
-            else:
-                print(f"\n({i} - {percent:0.1f}%)", end='', flush=True)
-            last_percent = percent
-        children = build_node_children(node)
-        for c in children:
-            if not c.only_one_possible():
-                next_nodes.append(c)
 
-    print('\n')
+    cpu_count = multiprocessing.cpu_count()
+
+    block_size = max(math.ceil(n / cpu_count), 100)
+    n_blocks = math.ceil(n / block_size)
+    assert n_blocks <= cpu_count
+
+    blocks = [
+        nodes[i * block_size: min((i + 1) * block_size, n)] for i in range(n_blocks)
+    ]
+
+    t0 = datetime.now()
+    with multiprocessing.Pool(cpu_count) as pool:
+        single = partial(find_best_candidates_for_block, scores_mem=ctx.shared_scores, n_words=ctx.n_words)
+        best_for_node_block = pool.map(single, blocks)
+    t1 = datetime.now()
+    best_for_node = [x for block in best_for_node_block for x in block]
+    t2 = datetime.now()
+
+    for node, best in zip(nodes, best_for_node):
+        add_candidates_to_node(node, best, next_nodes)
+    t3 = datetime.now()
+    print('Inner timings', (t1 - t0).total_seconds(), (t2 - t1).total_seconds(), (t3 - t2).total_seconds())
+
+    # for i, node in enumerate(nodes):
+    #     best = find_best_candidates(node, ctx.shared_scores, ctx.n_words)
+    #     children = add_candidates_to_node(node, best)
+    #     for c in children:
+    #         if not c.only_one_possible():
+    #             next_nodes.append(c)
+
+    elapsed = (datetime.now() - t0).total_seconds()
+    print(f'{n} nodes evaluated in {elapsed} seconds')
     return next_nodes
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(level='DEBUG')
+    LOGGER = logging.getLogger('analyze')
+    LOGGER.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    LOGGER.addHandler(handler)
+
     LOGGER.setLevel(logging.DEBUG)
-    LOGGER.info(f"# valid words = {N_WORDS}")
+    ctx = WordleContext()
+    LOGGER.info(f'READ {ctx.n_words} WORDS')
+    LOGGER.info(f'READ {len(ctx.outcomes)} PRE_CALCULATED OUTCOMES')
+    LOGGER.info(f'READ {len(ctx.scores)} PRE_CALCULATED SCORES')
 
     # Initialize the processing list
 
-    top = Node([i for i in range(0, N_WORDS)])
+    top = Node([i for i in range(0, ctx.n_words)])
     to_process = [top]
 
     next_process = []
 
     while to_process:
-        to_process = step_downward(to_process)
+        to_process = step_downward(to_process, ctx)
 
     top.keep_best()
 
-    top.display()
+    top.display(ctx)
